b50d3f7649534856197e54f6417440d59ac58198
Mateusz Polański <mp.mateusz.polanski@gmail.com>
2025-01-25 10:16:14 +0100

    Add button to render movia add form

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import MovieList from './MovieList';
 
 function App() {
     const [movies, setMovies] = useState([]);
+    const [displayAddMovieForm, toggleForm] = useState(false);
 
     return (
         <div className="container">
             <h1>My favourite movies to watch</h1>
             <MovieList movies={movies}/>
-            <MovieForm onMovieSubmit={(movie) => setMovies([...movies, movie])} buttonLabel="Add a movie"/>
+            {!displayAddMovieForm && <button onClick={() => toggleForm(true)}>Add movie</button>}
+            {displayAddMovieForm && <MovieForm onMovieSubmit={(movie) => setMovies([...movies, movie])} buttonLabel="Add a movie"/>}
         </div>
     );
 }
