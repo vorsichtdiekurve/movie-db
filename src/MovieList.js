29d8026009f6d574ea5865338cf5b71580fc1f29
Mateusz Polański <mp.mateusz.polanski@gmail.com>
2025-01-25 10:21:02 +0100

    Display info if no movie in list

--- src/MovieList.js
+++ src/MovieList.js
@@ -4,9 +4,12 @@ export default function MovieList(props) {
 
     return (
         <div><h2>Titles</h2>
+            {props.movies.length > 0 &&
             <ul>
                 {props.movies.map(movie => <Movie movie={movie}/>)}
             </ul>
+            }
+            {props.movies.length === 0 && <label>No movies in list!</label>}
         </div>
     );
 }
